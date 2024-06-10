4d62ce4a7bee3cd6a3f6891c8babb137d34e8ee1
MUHAMMAD HUSSAIN SHAMIM <142653978+hussainshamim16@users.noreply.github.com>
2024-06-09 20:12:03 -0700

    add more files

---
+++ user.js
@@ -0,0 +1,10 @@
+
+
+var Name = document.getElementById("Name")
+var Email = document.getElementById("Email")
+var couse = document.getElementById("couse")
+
+function sub(){
+   
+    console.log("red")
+}
