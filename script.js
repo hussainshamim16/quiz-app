50f6725af13f188ca6da8aecac77905149a6c8ee
MUHAMMAD HUSSAIN SHAMIM <hj422748@gmail.com>
2024-06-13 21:18:48 +0500

    changes in javaScript

--- script.js
+++ script.js
@@ -1,8 +1,45 @@
-var same =  "red";
-console.log(same)
 
-var food = "apple";
-var vegetable = "tammato";
-if(food < vegetable){
-    console.log("hy there");
+var q = [
+    {
+    id:1,
+    quiestion: "how Mani programing languages",
+    jawwab:{
+        a: 3,
+        b: 4 
+    },
+    answer: 3
+},
+{
+    id:2,
+    quiestion: "how Mani lead languages in programing",
+    jawwab:{
+        a: 1,
+        b: 3, 
+    },
+    answer: 3
+},
+]
+
+
+for (var key in q) {
+    if (q.hasOwnProperty.call(q, key)) {
+        console.log(q[key].quiestion);
+        
+    }
 }
+
+// console.log(q)
+
+var stdName = document.getElementById("stdName")
+var stdEmail = document.getElementById("stdEmail")
+var quizTime = document.getElementById("quizTime")
+var sawaal = document.getElementById("sawaal")
+
+// stdName.innerHTML = prompt("your  Name")
+// stdEmail.innerHTML = prompt("your  roll Number")
+quizTime.innerHTML = "01:00"
+
+
+
+
+
